9a558b5d75a70f1ec0d7858d4a695602ff3d0987
Spectrum5 <toskusergio@gmail.com>
2023-03-12 23:37:05 +0100

    Esercizio finito - Stampa DB senza style

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,5 +1,25 @@
-<section class="cards">
-    <div class="card">
+<h1>Movies</h1>
 
-    </div>
-</section>
+<div class="">
+    @foreach ($movies as $movie)
+        <div class="">
+            <div class="card-info">
+                <h3 class="title">
+                    {{ $movie->title }}
+                </h3>
+                <div class="original-title">
+                    {{ $movie->original_title }}
+                </div>
+                <div class="nationality">
+                    {{ $movie->nationality }}
+                </div>
+                <div class="date">
+                    {{ $movie->date }}
+                </div>
+                <div class="vote">
+                    {{ $movie->vote }}
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
