b36fb72dbfe3cf726592cadf38d4e265feb94736
Spectrum5 <toskusergio@gmail.com>
2023-03-12 23:43:47 +0100

    Inizio aggiunta style

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,25 +1,30 @@
-<h1>Movies</h1>
+<div class="container text-center">
+    <h1>
+        Movies
+    </h1>
 
-<div class="">
-    @foreach ($movies as $movie)
-        <div class="">
-            <div class="card-info">
-                <h3 class="title">
-                    {{ $movie->title }}
-                </h3>
-                <div class="original-title">
-                    {{ $movie->original_title }}
-                </div>
-                <div class="nationality">
-                    {{ $movie->nationality }}
-                </div>
-                <div class="date">
-                    {{ $movie->date }}
-                </div>
-                <div class="vote">
-                    {{ $movie->vote }}
+    <div class="row justify-content-center gap-3">
+        @foreach ($movies as $movie)
+            <div class="col-2">
+                <div class="card-info">
+                    <h3 class="title">
+                        {{ $movie->title }}
+                    </h3>
+                    <div class="original-title">
+                        {{ $movie->original_title }}
+                    </div>
+                    <div class="nationality">
+                        {{ $movie->nationality }}
+                    </div>
+                    <div class="date">
+                        {{ $movie->date }}
+                    </div>
+                    <div class="vote">
+                        {{ $movie->vote }}
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
+
 </div>
